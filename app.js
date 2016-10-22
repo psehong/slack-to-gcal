ecb0a1c430fb420bd7e278ef9d3fdedbb8873676
Peter Hong <psehong@gmail.com>
2016-10-22 14:43:44 -0400

    Add tomorrow

--- app.js
+++ app.js
@@ -186,6 +186,11 @@ const initClients = () => {
         const range = appUtil.gcalUpcomingRange(googleClient.GCAL_DATE_FORMAT);
         log.info(`Query, start: ${range.start}, end: ${range.end}`);
         listGcalEvents(getEvent, range.start, range.end, slackWebClient, slackMessage.channel);
+      } else if (slackMessage.text.split(SLACK_AT_BOT)[1].trim().toLowerCase() === 'tmrw' ||
+          slackMessage.text.split(SLACK_AT_BOT)[1].trim().toLowerCase() === 'tomorrow') {
+        const range = appUtil.gcalTmrwRange(googleClient.GCAL_DATE_FORMAT);
+        log.info(`Query, start: ${range.start}, end: ${range.end}`);
+        listGcalEvents(getEvent, range.start, range.end, slackWebClient, slackMessage.channel);
       } else {
         if (appUtil.hasTime(slackMessage.text)) {
           log.info(`SlackMessage found to have time: ${slackMessage.text}`);
